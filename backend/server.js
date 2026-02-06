f7ef3cb3b54bd676bfe4e1b23242118d73dec8ef
angelgabrielleos826-ui <angelgabrielleos826@gmail.com>
2026-02-05 18:22:55 -0600

    Mi nueva rama

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors= require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require("./data/src/routes/auth.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//health check 
+app.get("/health", (req, res) => {
+    res.json({ ok: true, ts: new Date().toISOString});
+});
+
+//Routes
+app.use("/api/auth", authRoutes);
+
+//Empty Error
+app.use((req, res) =>{
+    res.status(404).json({ error: "Not found "});
+});
+
+async function start() {
+    const port = process.env.PORT || 3000
+
+    if (!process.env.MONGO_URI) throw new Error ("MONGO_URI no esta configurado");
+    if (!process.env.JWT_SECRET) throw new Error ("JWT_SECREST no configurado");
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo conectado");
+
+    app.listen(port, ()=> console.log(`API exitosamente ejecutada http://localhost:${port}`));
+
+};
+
+start().catch((err)=>{
+    console.error("Error al iniciar", err.message);
+    process.exit(1);
+});
+
